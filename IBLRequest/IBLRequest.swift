f265b2768beaca9ed9babbd49657589841d1135e
Chris Winstanley <chris.winstanley@bbc.co.uk>
2018-07-30 11:39:42 +0100

    Added jazzy docs for IBLRequest

--- IBLRequest/IBLRequest.swift
+++ IBLRequest/IBLRequest.swift
@@ -8,13 +8,21 @@
 
 import Foundation
 
+/// Public interface to the IBLRequest framework.
 public struct IBLRequest {
     
+    /// Request categories from IBL, performs external request.
+    ///
+    /// - Parameter completion: Completion block fired when request completes, returns an array of `IBLCategory`.
     static public func getCategories(completion: @escaping ([IBLCategory]) -> ()) {
         let urlSessionCategoryProvider = URLSessionCategoryProvider(url: CategoryURLBuilder().create(), parser: JSONCategoryParser())
         urlSessionCategoryProvider.getCategories(completion: completion)
     }
     
+    ///  Request `IBLEpisode`s for a given `IBLCategory` from IBL, performs external request.
+    ///
+    /// - Parameter categoryID: Provide the `IBLCategory.id` of the category you want `IBLEpisode`'s for.
+    /// - Parameter completion: Completion block fired when request completes, returns an array of `IBLEpisode`.
     static public func getEpisodes(for categoryID: String, completion: @escaping ([IBLEpisode]) -> ()) {
         let urlSessionEpisodeProvider = URLSessionEpisodeProvider(url: EpisodesURLBuilder(categoryID: categoryID).create(), parser: JSONEpisodeParser())
         urlSessionEpisodeProvider.getEpisodes(completion: completion)
